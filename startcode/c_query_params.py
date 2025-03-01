9267fe3e414c1de078cee1cd67a3f15cabc970d3
Ac16ll04ig4 <ameriecallegari@hotmail.com>
2025-03-01 10:45:16 +0100

    P2_Start_L2

--- startcode/c_query_params.py
+++ startcode/c_query_params.py
@@ -1,4 +1,14 @@
-# Maak een programma dat:
-# - De naam van de gebruiker vraagt.
-# - De naam van de gebruiker doorstuurt naar de API.
-# - Een tekstje print met de naam en de voorspelde leeftijd.
+import requests
+
+naam = input("Wat is je naam? ")
+
+response = requests.get(
+    url='https://api.agify.io/',
+    params={'name': naam}
+    )
+if response.status_code == 200:
+    data = response.json()
+    print(data['age'])
+else:
+    print(f'error code{response.status_code}')
+
